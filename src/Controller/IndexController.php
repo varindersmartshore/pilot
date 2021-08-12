724aa2e7a73afa2ccb746398e26b257c67e506e2
Varinder Kaur <varinder@smartshore.nl>
2021-08-12 17:58:44 +0530

    Add asset library

---
+++ src/Controller/IndexController.php
@@ -0,0 +1,19 @@
+<?php
+// src/Controller/IndexController.php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class IndexController extends AbstractController
+{
+    public function index()
+    {
+        $your_name = "Welcome!";
+        return $this->render('index.html.twig', [
+            'your_name' => $your_name,
+        ]);
+    }
+}
+?>
