f3c13f79c254445b6cc60f9007e42cfed50d6947
Varinder Kaur <varinder@smartshore.nl>
2021-08-13 18:26:22 +0530

    make controller, entity and migration

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,6 +2,7 @@
 // src/Controller/IndexController.php
 namespace App\Controller;
 
+use Doctrine\DBAL\Driver\Connection;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
