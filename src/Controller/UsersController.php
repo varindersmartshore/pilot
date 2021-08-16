36e8ae6b1436e156b7e2e24058be721e27c1a094
Varinder Kaur <varinder@smartshore.nl>
2021-08-16 17:05:24 +0530

    CRUD of users module

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -2,19 +2,98 @@
 
 namespace App\Controller;
 
+use App\Entity\Users;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class UsersController extends AbstractController
 {
-    /**
-     * @Route("/users", name="users")
-     */
-    public function index(): Response
+    public function index()
     {
-        return $this->render('users/index.html.twig', [
-            'controller_name' => 'UsersController',
+        $repository = $this->getDoctrine()->getRepository(Users::class);
+        $users = $repository->findBy([
+            'status' => 1,
         ]);
+        return $this->render('users/index.html.twig', [
+            'users' => $users,
+        ]);
+    }
+
+    public function addUser()
+    {
+        $repository = $this->getDoctrine()->getRepository(Users::class);
+        $users = $repository->findBy([
+            'status' => 1,
+        ]);
+        return $this->render('users/add.html.twig');
+    } 
+
+    public function insertUser(Request $request, ValidatorInterface $validator, EntityManagerInterface $entityManager)
+    {
+        $users = new Users;
+        $errors = $validator->validate($users);
+        if (count($errors) > 0) {
+            $errorsString = (string) $errors;
+            return new Response($errorsString);
+        } else {
+            $users->setUsername($request->request->get('username'));
+            $users->setPassword($request->request->get('password'));
+            $users->setEmail($request->request->get('email'));
+            $users->setStatus(1);
+            $entityManager->persist($users);
+            $entityManager->flush();
+            return $this->redirectToRoute('users',array('message' => 'The user is added!'));
+        }
+        return new Response('Something went wrong!');
+    }
+
+    public function editUser(int $id)
+    {
+        $repository = $this->getDoctrine()->getRepository(Users::class);
+        $user = $repository->findOneBy([
+            'id' => $id,
+        ]);
+        return $this->render('users/edit.html.twig',[
+            'user' => $user
+        ]);
+    } 
+
+    public function updateUser(Request $request, ValidatorInterface $validator, EntityManagerInterface $entityManager)
+    {
+        $users = new Users;
+        $errors = $validator->validate($users);
+        if (count($errors) > 0) {
+            $errorsString = (string) $errors;
+            return new Response($errorsString);
+        } else {
+            $user = $this->getDoctrine()->getRepository(Users::class)->findOneBy(['id'=>$request->request->get('id')]);
+            $user->setUsername($request->request->get('username'));
+            $user->setEmail($request->request->get('email'));
+            $entityManager->persist($user);
+            $entityManager->flush();
+            return $this->redirectToRoute('users',array('message' => 'The user is updated!'));
+        }
+        return new Response('Something went wrong!');
+    }
+    
+    public function deleteUser(int $id, EntityManagerInterface $entityManager)
+    {
+        $repository = $this->getDoctrine()->getRepository(Users::class);
+        $user = $repository->findOneBy([
+            'id' => $id,
+        ]);
+        if (!empty($user)) {
+            $entityManager->remove($user);
+            $entityManager->flush();
+            return $this->redirectToRoute('users',array('message' => 'The user is deleted!'));
+        } else {
+            return $this->redirectToRoute('users',array('message' => 'The user is not deleted!'));
+        }
+        
+        
     }
 }
