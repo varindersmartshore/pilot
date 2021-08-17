2372c53991f38eec13821df7fdffd8785ca12e7d
Varinder Kaur <varinder@smartshore.nl>
2021-08-17 14:39:23 +0530

    Implement csrf token in forms

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -8,10 +8,14 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\PropertyAccess\PropertyAccess;
+use Symfony\Component\Validator\Constraints as Assert;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
+use Psr\Log\LoggerInterface;
 
 class UsersController extends AbstractController
 {
+    // Display list of users
     public function index()
     {
         $repository = $this->getDoctrine()->getRepository(Users::class);
@@ -23,16 +27,14 @@ class UsersController extends AbstractController
         ]);
     }
 
+    // Display add user page
     public function addUser()
     {
-        $repository = $this->getDoctrine()->getRepository(Users::class);
-        $users = $repository->findBy([
-            'status' => 1,
-        ]);
         return $this->render('users/add.html.twig');
     } 
 
-    public function insertUser(Request $request, ValidatorInterface $validator, EntityManagerInterface $entityManager)
+    // Insert new user in DB
+    public function insertUser(Request $request, ValidatorInterface $validator, EntityManagerInterface $entityManager, LoggerInterface $logger)
     {
         $users = new Users;
         $errors = $validator->validate($users);
@@ -40,18 +42,24 @@ class UsersController extends AbstractController
             $errorsString = (string) $errors;
             return new Response($errorsString);
         } else {
-            $users->setUsername($request->request->get('username'));
-            $users->setPassword($request->request->get('password'));
-            $users->setEmail($request->request->get('email'));
-            $users->setStatus(1);
-            $entityManager->persist($users);
-            $entityManager->flush();
-            $this->addFlash('success', 'The user is added!');
-            return $this->redirectToRoute('users');
+            $submittedToken = $request->request->get('_csrf_token');
+            if ($this->isCsrfTokenValid('authenticate_add_user', $submittedToken)) {
+                $users->setUsername($request->request->get('username'));
+                $users->setPassword($request->request->get('password'));
+                $users->setEmail($request->request->get('email'));
+                $users->setStatus(1);
+                $entityManager->persist($users);
+                $entityManager->flush();
+                $this->addFlash('success', 'The user is added!');
+                return $this->redirectToRoute('users');
+            } else {
+                return new Response('Invalid Token!');
+            }
         }
         return new Response('Something went wrong!');
     }
 
+    // Display edit user page with specific id
     public function editUser(int $id)
     {
         $repository = $this->getDoctrine()->getRepository(Users::class);
@@ -63,6 +71,7 @@ class UsersController extends AbstractController
         ]);
     } 
 
+    // Update an existing user
     public function updateUser(Request $request, ValidatorInterface $validator, EntityManagerInterface $entityManager)
     {
         $users = new Users;
@@ -71,17 +80,23 @@ class UsersController extends AbstractController
             $errorsString = (string) $errors;
             return new Response($errorsString);
         } else {
-            $user = $this->getDoctrine()->getRepository(Users::class)->findOneBy(['id'=>$request->request->get('id')]);
-            $user->setUsername($request->request->get('username'));
-            $user->setEmail($request->request->get('email'));
-            $entityManager->persist($user);
-            $entityManager->flush();
-            $this->addFlash('success', 'The user is updated!');
-            return $this->redirectToRoute('users');
+            $submittedToken = $request->request->get('_csrf_token');
+            if ($this->isCsrfTokenValid('authenticate_edit_user', $submittedToken)) {
+                $user = $this->getDoctrine()->getRepository(Users::class)->findOneBy(['id'=>$request->request->get('id')]);
+                $user->setUsername($request->request->get('username'));
+                $user->setEmail($request->request->get('email'));
+                $entityManager->persist($user);
+                $entityManager->flush();
+                $this->addFlash('success', 'The user is updated!');
+                return $this->redirectToRoute('users');
+            } else {
+                return new Response('Invalid Token!');
+            }
         }
         return new Response('Something went wrong!');
     }
     
+    // Delete existing user
     public function deleteUser(int $id, EntityManagerInterface $entityManager)
     {
         $repository = $this->getDoctrine()->getRepository(Users::class);
@@ -94,10 +109,8 @@ class UsersController extends AbstractController
             $this->addFlash('success', 'The user is deleted!');
             return $this->redirectToRoute('users');
         } else {
-            $this->addFlash('failed', 'The user is not deleted!');
+            $this->addFlash('failed', 'The user not found!');
             return $this->redirectToRoute('users');
         }
-        
-        
     }
 }
