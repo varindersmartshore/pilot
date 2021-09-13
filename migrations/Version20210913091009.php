735a384e1cd1f3e27d4f2e37a69e572d5215a4c6
Ankit Tara <ankit@smartshore.nl>
2021-09-13 14:43:01 +0530

    Clone project in laptop

--- migrations/Version20210913091009.php
+++ migrations/Version20210913091009.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210907082254 extends AbstractMigration
+final class Version20210913091009 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -22,19 +22,20 @@ final class Version20210907082254 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE items (id INT AUTO_INCREMENT NOT NULL, list_id_id INT NOT NULL, item_name VARCHAR(255) NOT NULL, color VARCHAR(255) DEFAULT NULL, order_by NUMERIC(10, 0) NOT NULL, INDEX IDX_E11EE94DA6D70A54 (list_id_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE lists (id INT AUTO_INCREMENT NOT NULL, user_id INT NOT NULL, list_name VARCHAR(255) NOT NULL, INDEX IDX_8269FA5A76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, profile VARCHAR(255) DEFAULT NULL, is_verified TINYINT(1) NOT NULL, verification_token VARCHAR(255) DEFAULT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE users (id INT AUTO_INCREMENT NOT NULL, username VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, status INT DEFAULT 1 NOT NULL, profile VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE items ADD CONSTRAINT FK_E11EE94DA6D70A54 FOREIGN KEY (list_id_id) REFERENCES lists (id)');
         $this->addSql('ALTER TABLE lists ADD CONSTRAINT FK_8269FA5A76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
-        $this->addSql('ALTER TABLE user ADD password VARCHAR(255) NOT NULL, ADD is_verified TINYINT(1) NOT NULL, ADD verification_token VARCHAR(255) DEFAULT NULL');
-        $this->addSql('ALTER TABLE users DROP password');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE items DROP FOREIGN KEY FK_E11EE94DA6D70A54');
+        $this->addSql('ALTER TABLE lists DROP FOREIGN KEY FK_8269FA5A76ED395');
         $this->addSql('DROP TABLE items');
         $this->addSql('DROP TABLE lists');
-        $this->addSql('ALTER TABLE user DROP password, DROP is_verified, DROP verification_token');
-        $this->addSql('ALTER TABLE users ADD password VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`');
+        $this->addSql('DROP TABLE user');
+        $this->addSql('DROP TABLE users');
     }
 }
--- src/Entity/Lists.php
+++ src/Entity/Lists.php
@@ -7,8 +7,6 @@ use App\Repository\ItemsRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
-use Doctrine\Common\Persistence\ManagerRegistry;
-use App\Service\ItemSort;
 use Symfony\Component\Validator\Constraints as Assert;
 
 /**
@@ -40,10 +38,9 @@ class Lists
      */
     private $user;
 
-    public function __construct(ManagerRegistry $registry)
+    public function __construct()
     {
         $this->items = new ArrayCollection();
-        $this->em = $registry;
     }
 
     public function getId(): ?int
